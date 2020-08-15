d202cffbc04a9b5370578959e3d597f4da7417c9
Gary Dohmeier <>
2020-08-15 08:09:04 -0400

    trying with postgress

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL
+  mysqlDatabaseUrl: 'postgres://tkeuzrafbjwbdi:601e8610836775d21092ccd5fcab9422071c1a18ff89c6695d2c2f12f89d8d36@ec2-3-208-50-226.compute-1.amazonaws.com:5432/dekiehg6o4nn5u'
+
 }
